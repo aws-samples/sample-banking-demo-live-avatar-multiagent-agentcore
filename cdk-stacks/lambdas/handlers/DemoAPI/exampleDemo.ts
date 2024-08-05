524890920b4aa7d1863a7e4df42a24d430277937
Sagar Khasnis <khasnis@amazon.com>
2024-08-05 14:59:10 -0700

    fix linting issues

--- cdk-stacks/lambdas/handlers/DemoAPI/exampleDemo.ts
+++ cdk-stacks/lambdas/handlers/DemoAPI/exampleDemo.ts
@@ -1,22 +1,22 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const LambdaUtility = require('../../lib/LambdaUtility');
+import { buildLambdaResponse } from '../../lib/LambdaUtility';
 import { Context, APIGatewayEvent } from 'aws-lambda';
 
 exports.handler = async (event: APIGatewayEvent, context: Context) => {
 
-    try {
-        console.info("App Version:", process.env.APPLICATION_VERSION)
-        console.debug(`Event: `, event);
+  try {
+    console.info("App Version:", process.env.APPLICATION_VERSION)
+    console.debug(`Event: `, event);
 
-        return LambdaUtility.buildLambdaResponse(context, 201, {
-          success: 'Create Demo succeeded!',
-          data: {},
-        });
-    }
-    catch (error: any) {
-        console.error(error);
-        return LambdaUtility.buildLambdaResponse(context, error.statusCode || 500, { message: error.message });
-    }
+    return buildLambdaResponse(context, 201, {
+      success: 'Create Demo succeeded!',
+      data: {},
+    });
+  }
+  catch (error: any) {
+    console.error(error);
+    return buildLambdaResponse(context, error.statusCode || 500, { message: error.message });
+  }
 }
--- cdk-stacks/lambdas/lib/CommonUtility.ts
+++ cdk-stacks/lambdas/lib/CommonUtility.ts
@@ -1,43 +1,46 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+import * as crypto from 'crypto';
 
 export const uuid = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = crypto.randomBytes(1)[0] & 0x0f;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
         return v.toString(16);
-    });
+      });
 }
 
 export const validateEmailAddress = (email: string): boolean => {
-    const emailRegEx = /^[^\s@]+@[^\s@]+$/
-    return emailRegEx.test(email)
+  const emailRegEx = /^[^\s@]+@[^\s@]+$/
+  return emailRegEx.test(email)
 }
 
 export const makeComparator = (key: string, order: 'asc' | 'desc' = 'asc') => {
-    return (a: any, b: any) => {
-        if (!Object.prototype.hasOwnProperty.call(a, key) || !Object.prototype.hasOwnProperty.call(b, key)) return 0;
+  return (a: any, b: any) => {
+    if (!Object.prototype.hasOwnProperty.call(a, key) || !Object.prototype.hasOwnProperty.call(b, key)) return 0;
 
-        const aVal = ((typeof a[key] === 'string') ? a[key].toUpperCase() : a[key]);
-        const bVal = ((typeof b[key] === 'string') ? b[key].toUpperCase() : b[key]);
+    const aVal = ((typeof a[key] === 'string') ? a[key].toUpperCase() : a[key]);
+    const bVal = ((typeof b[key] === 'string') ? b[key].toUpperCase() : b[key]);
 
-        let comparison = 0;
-        if (aVal > bVal) comparison = 1;
-        if (aVal < bVal) comparison = -1;
+    let comparison = 0;
+    if (aVal > bVal) comparison = 1;
+    if (aVal < bVal) comparison = -1;
 
-        return order === 'desc' ? (comparison * -1) : comparison
-    };
+    return order === 'desc' ? (comparison * -1) : comparison
+  };
 }
 
 export const wait = (time: number): Promise<void> => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(), time);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), time);
+  });
 }
 
 export const customBackoff = (retryCount: number): number => {
-    const timeToWait = 2 ** (retryCount+1) * 1000;
-    const jitter = Math.floor(Math.random() * (1000 - 100 + 1) + 100)
-    const waitWithJitter = timeToWait + jitter
-    console.debug(`retry count: ${retryCount}, timeToWait: ${timeToWait}, jitter: ${jitter} waiting: ${waitWithJitter}ms`)
-    return waitWithJitter
-}
+    const timeToWait = 2 ** (retryCount + 1) * 1000;
+    const jitterBytes = crypto.randomBytes(1);
+    const jitter = (jitterBytes[0] / 255) * (1000 - 100) + 100;
+    const waitWithJitter = timeToWait + Math.floor(jitter);
+    console.debug(`retry count: ${retryCount}, timeToWait: ${timeToWait}, jitter: ${jitter} waiting: ${waitWithJitter}ms`);
+    return waitWithJitter;
+  };
