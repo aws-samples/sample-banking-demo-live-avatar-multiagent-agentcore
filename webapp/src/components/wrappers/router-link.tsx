eb80a7869352348eee22f8fdb51d652b5533e1ab
Tamil Jayakumar <tamjay@amazon.com>
2025-01-20 15:36:30 -0800

    refactor(app): :wastebasket: clean repo
    prepared the repo to bring in v2-alpha version

--- webapp/src/components/wrappers/router-link.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from '@cloudscape-design/components'
-import { useOnFollow } from '../../hooks/use-on-follow'
-
-export default function RouterLink(props: any) {
-  const onFollow = useOnFollow()
-
-  return <Link {...props} onFollow={onFollow} />
-}
