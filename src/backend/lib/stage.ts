33b8dc3c999b25af6661ba56eb4f27bcac2aed40
Chris Azer <azer@amazon.com>
2025-03-04 17:51:58 -0800

    feat(simulation): added simulation

--- src/backend/lib/stage.ts
+++ src/backend/lib/stage.ts
@@ -9,6 +9,7 @@ import { GraphApiStack } from "./stacks/graph-api";
 import { RestApiStack } from "./stacks/rest-api";
 import { StorageHydrateStack, StorageStack } from "./stacks/storage";
 import { VpcStack } from "./stacks/vpc";
+import { SimulationStack } from "./stacks/simulation/simulation";
 
 export class ApplicationStage extends Stage {
     constructor(scope: Construct, id: string, props: StageProps) {
@@ -54,6 +55,8 @@ export class ApplicationStage extends Stage {
         new StorageHydrateStack(this, "storageHydrate", {
             storageBucket: storageStack.storageBucket,
         });
+
+        new SimulationStack(this, "simulationStack", {});
 
         // this stack must be named frontendBuild
         new FrontendBuildStack(this, "frontendBuild", {
